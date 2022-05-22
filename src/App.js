92902530c7048e6981c59ceadd37848eced5a125
nodejs.github <nodejs.github>
2022-05-22 18:04:00 +0200

    -my movie footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie footer </h1>
+
+
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {FaFire} from "react-icons/fa";
+import {FaYoutube } from "react-icons/fa";
+import {FaDiscord } from "react-icons/fa";
+import {FaCloudUploadAlt} from "react-icons/fa";
+
+const Footer = () => {
+     return ( 
+          <div>
+               
+          <div className="Footer">
+
+ <p className='head'> < FaFire className='icon1' />Favorites</p>
+
+ <h1 className='head1'>THE Box Office</h1>
+ <p className='paras'>Set In A Beautiful Seaside Town On The Italian Riviera , The Original Animated Features Is Acoming-Of-Age
+      Story About One young Boy Ezperiencing An Unforgettable Summer Filled With Gelato , Pasta And Endless Scooter Rides.
+      <br />
+
+     <FaYoutube className='icon'/>
+     <FaDiscord className='icon'/>
+     <FaCloudUploadAlt className='icon'/>
+ </p>
+ <div className='content'>
+
+               <div className='direction'>
+               <h5 className='theme'>COMAPANY</h5>
+               <li><a href='/'>Our Team</a></li>
+               <li><a href='/'>Our Career</a></li>
+               <li><a href='/'>Our Partners</a></li>
+               <li><a href='/'>Charity</a></li>
+               <li><a href='/'>Press Room</a></li>
+               <li><a href='/'>Plex Lab</a></li>
+
+          </div>
+
+          <div className='direction'>
+               <h5 className='theme'>WATCH</h5>
+               <li><a href='/'>Movies</a></li>
+               <li><a href='/'>TV Shows</a></li>
+               <li><a href='/'> Commedy </a></li>
+                <li><a href='/'>Cartoons</a></li>
+               <li><a href='/'>Horror</a></li>
+               <li><a href='/'>Funny Shows</a></li>
+
+          </div>
+          
+
+          <div className='direction'>
+               <h5 className='theme'>HELP</h5>
+               <li><a href='/'>About Us </a></li>
+               <li><a href='/'>Customer Support</a></li>
+               <li><a href='/'>Contact Us</a></li>
+               <li><a href='/'>Advertise</a></li>
+               <li><a href='/'>Forums</a></li>
+               <li><a href='/'>Others</a></li>
+</div>
+
+
+        <p className='end'>All Right Reserved @ 2022 The Associates &&  Accessibility  </p>
+        <p className='end1'> Language :<span className='language'>  English (US)</span></p>
+     </div>
+          </div>
+          </div>
+      );
+}
+ 
+export default Footer;
