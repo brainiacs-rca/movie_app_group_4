b84cdb4a7c207ad59949f480ddf786e12cf9bba4
u-Alice <umugwanezaalice22@gmail.com>
2022-06-18 16:24:53 +0200

    merged with the first branch

---
+++ brainiacs/src/App.js
@@ -0,0 +1,12 @@
+import Landpage from "./components/landpage";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Landpage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
